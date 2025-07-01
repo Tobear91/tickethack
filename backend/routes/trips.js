da0a980b6cf16233f9ac736208a7d298d49fb6f8
CamilleHurtaud <camille.hurtaud@gmail.com>
2025-07-01 12:32:43 +0200

    Ajout de la route POST pour récupérer des trips en fonction de arrival, departure et date

--- backend/routes/trips.js
+++ backend/routes/trips.js
@@ -2,9 +2,23 @@ const express = require("express");
 const router = express.Router();
 const Trip = require("../models/trips");
 
+// Get all trips (pas sur d'etre utile en front)
 router.get("/", async (req, res) => {
   try {
     const trips = await Trip.find();
+    if (trips.length === 0) throw new Error("Trips not found");
+    res.json({ result: true, trips });
+  } catch (error) {
+    res.status(500).json({ result: false, error: error.message });
+  }
+});
+
+// Get trips by departure, arrival and date
+router.post("/", async (req, res) => {
+  try {
+    const datas = ({ departure, arrival, date } = req.body);
+    const trips = await Trip.find(datas);
+    if (trips.length === 0) throw new Error("Trips not found");
     res.json({ result: true, trips });
   } catch (error) {
     res.status(500).json({ result: false, error: error.message });
