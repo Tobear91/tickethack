cffaeec110838ab31e07182dbbcf4a488a457673
CamilleHurtaud <camille.hurtaud@gmail.com>
2025-07-01 15:03:18 +0200

    Modification pour faire le find en fonction du format de la data + test des champs dans la route POST avec un module

--- backend/routes/trips.js
+++ backend/routes/trips.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const moment = require("moment");
 const Trip = require("../models/trips");
+const { checkBody } = require("../modules/checkBody");
 
 // Get all trips (pas sur d'etre utile en front)
 router.get("/", async (req, res) => {
@@ -16,8 +18,22 @@ router.get("/", async (req, res) => {
 // Get trips by departure, arrival and date
 router.post("/", async (req, res) => {
   try {
-    const datas = ({ departure, arrival, date } = req.body);
-    const trips = await Trip.find(datas);
+    if (!checkBody(req.body, ["departure", "arrival", "date"]))
+      throw new Error("Missing or empty fields");
+
+    const { departure, arrival, date } = req.body;
+    const $gte = moment.utc(date, "YYYY-MM-DD").startOf("day").toDate();
+    const $lte = moment.utc(date, "YYYY-MM-DD").endOf("day").toDate();
+
+    const trips = await Trip.find({
+      departure,
+      arrival,
+      date: {
+        $gte,
+        $lte,
+      },
+    });
+
     if (trips.length === 0) throw new Error("Trips not found");
     res.json({ result: true, trips });
   } catch (error) {
